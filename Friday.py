f67f29f98b3dfc21413d8034b2bd0b884afe8ccf
Denise Byrnes <dbyrnes@wooster.edu>
2016-09-05 16:39:26 -0400

    Added a print(s)

---
+++ Friday.py
@@ -0,0 +1,9 @@
+//dbyrnes
+def addTwoInt(a, b):
+  print a+b
+
+
+def pickAndPlay():
+  fileName = pickAFile()
+  sound = makeSound(fileName)
+  play(sound)
--- quine.py
+++ quine.py
@@ -1,2 +1,4 @@
+//Zacks file
 s = 's = %r\nprint(s%%s)'
 print(s%s)
+print(s)
